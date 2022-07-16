bd538e5780a9bd60a880041fe62c2605ca4fb0db
Alin Panaitiu <alin.panaitiu@gmail.com>
2022-07-16 17:23:23 +0300

    Initial Commit

---
+++ ClipOptim/ClipOptimApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipOptimApp.swift
+//  ClipOptim
+//
+//  Created by Alin Panaitiu on 16.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ClipOptimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipOptim/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ClipOptim
+//
+//  Created by Alin Panaitiu on 16.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
