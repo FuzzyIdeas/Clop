0f0911d9fa91b74e602a588d6ef1dab7df6e3b58
Alin Panaitiu <alin.panaitiu@gmail.com>
2023-12-07 17:04:30 +0200

    Fix app hangs

--- Clop/Xattr.swift
+++ Clop/Xattr.swift
@@ -6,6 +6,17 @@ enum Xattr {
         let localizedDescription = String(utf8String: strerror(errno))
     }
 
+    static let queue = DispatchQueue(label: "\(Bundle.main.bundleIdentifier ?? "com.lowtechguys.Clop").xattr", attributes: .concurrent)
+
+    static func withTimeout(_ timeout: TimeInterval, _ block: @escaping () -> Void) {
+        let workItem = DispatchWorkItem(block: block)
+        queue.async(execute: workItem)
+        let result = workItem.wait(timeout: .now() + timeout)
+        if result == .timedOut {
+            log.error("Xattr timed out")
+            workItem.cancel()
+        }
+    }
     /**
        Set extended attribute at path
 
@@ -15,7 +26,17 @@ enum Xattr {
          - path: Path to file, directory, symlink etc
      */
     static func set(named name: String, data: Data, atPath path: String) throws {
-        if setxattr(path, name, (data as NSData).bytes, data.count, 0, 0) == -1 { throw Error() }
+        var error: Swift.Error?
+
+        withTimeout(5) {
+            if setxattr(path, name, (data as NSData).bytes, data.count, 0, 0) == -1 {
+                error = Error()
+            }
+        }
+
+        if let error {
+            throw error
+        }
     }
 
     /**
@@ -26,7 +47,17 @@ enum Xattr {
        - path: Path to file, directory, symlink etc
      */
     static func remove(named name: String, atPath path: String) throws {
-        if removexattr(path, name, 0) == -1 { throw Error() }
+        var error: Swift.Error?
+
+        withTimeout(5) {
+            if removexattr(path, name, 0) == -1 {
+                error = Error()
+            }
+        }
+
+        if let error {
+            throw error
+        }
     }
 
     /**
@@ -37,11 +68,27 @@ enum Xattr {
          - path: Path to file, directory, symlink etc
      */
     static func dataFor(named name: String, atPath path: String) throws -> Data {
-        let bufLength = getxattr(path, name, nil, 0, 0, 0)
+        var error: Swift.Error?
+        var data: Data?
 
-        guard bufLength != -1, let buf = malloc(bufLength), getxattr(path, name, buf, bufLength, 0, 0) != -1 else { throw Error() }
+        withTimeout(5) {
+            let bufLength = getxattr(path, name, nil, 0, 0, 0)
 
-        return Data(bytes: buf, count: bufLength)
+            guard bufLength != -1, let buf = malloc(bufLength), getxattr(path, name, buf, bufLength, 0, 0) != -1 else {
+                error = Error()
+                return
+            }
+
+            data = Data(bytes: buf, count: bufLength)
+        }
+
+        if let error {
+            throw error
+        }
+        guard let data else {
+            throw Error()
+        }
+        return data
     }
 
     /**
@@ -51,18 +98,32 @@ enum Xattr {
          - path: Path to file, directory, symlink etc
      */
     static func names(atPath path: String) throws -> [String]? {
-        let bufLength = listxattr(path, nil, 0, 0)
+        var error: Swift.Error?
+        var data: [String]?
+        withTimeout(5) {
+            let bufLength = listxattr(path, nil, 0, 0)
+            guard bufLength != -1 else {
+                error = Error()
+                return
+            }
 
-        guard bufLength != -1 else { throw Error() }
+            let buf = UnsafeMutablePointer<Int8>.allocate(capacity: bufLength)
+            guard listxattr(path, buf, bufLength, 0) != -1 else {
+                error = Error()
+                return
+            }
 
-        let buf = UnsafeMutablePointer<Int8>.allocate(capacity: bufLength)
+            var names = NSString(bytes: buf, length: bufLength, encoding: String.Encoding.utf8.rawValue)?.components(separatedBy: "\0")
+            names?.removeLast()
+            data = names
+        }
 
-        guard listxattr(path, buf, bufLength, 0) != -1 else { throw Error() }
-
-        var names = NSString(bytes: buf, length: bufLength, encoding: String.Encoding.utf8.rawValue)?.components(separatedBy: "\0")
-
-        names?.removeLast()
-
-        return names
+        if let error {
+            throw error
+        }
+        guard let data else {
+            throw Error()
+        }
+        return data
     }
 }
